0eecd4770ded4045e533fa9ae874100a1adabb5b
shettynuthan9-hash <shettynuthan9@gmail.com>
2026-08-11 17:58:24 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
       int n = nums.size();
-      if(n == 1)return nums[0];
-      if(nums[0]!=nums[1])return nums[0];
-      if(nums[n-1]!=nums[n-2])return nums[n-1];
-      int left = 0;
-      int right = n-1;
-      while(left<=right){
-        int mid = (left+right)/2;
-        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+      int low = 0;
+      int high = n-1;
+      if(n==1){
+        return nums[0];
+      }
+      if(nums[0]!=nums[1]){return nums[0];}
+      if(nums[n-1]!=nums[n-2]){
+        return nums[n-1];
+      }
+      while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[mid-1]!=nums[mid] && nums[mid+1]!=nums[mid]){
             return nums[mid];
         }
-        if((mid%2 ==0 && nums[mid]==nums[mid-1]) || 
-        (mid%2 != 0 && nums[mid]==nums[mid+1]) ){
-            right = mid - 1;
+        if((mid%2==0 && nums[mid-1]==nums[mid]) || (mid%2!=0 && nums[mid+1]==nums[mid])){
+            high = mid-1;
         }
         else{
-            left = mid + 1;
+            low = mid+1;
         }
-      } 
-      return -1; 
+      }
+      return -1;  
     }
 };
